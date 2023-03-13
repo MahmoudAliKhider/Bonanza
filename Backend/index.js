8132f8275760be9bdbe4ed4f737c34e014bc0330
MahmoudAliKhider <mahmoudkhider37@gmail.com>
2023-03-14 00:07:05 +0200

    connection to Database

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,27 @@
-const express = require('express')
+const express = require("express");
 const app = express();
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 
+require("dotenv/config");
 
-const port = 4000
-app.listen(()=>{
-    console.log(`server is running  http://localhost:${port}`)
-})
+app.use(morgan("tiny"));
+app.use(express.json());
+
+api = process.env.API;
+
+mongoose
+  .connect(process.env.CONNECTION,{
+    useNewUrlParser : true
+  })
+  .then(() => {
+    console.log("Database connected ...");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const port = 4000;
+app.listen(() => {
+  console.log(`server is running  http://localhost:${port}`);
+});
