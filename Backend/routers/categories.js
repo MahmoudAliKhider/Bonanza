99010b925cbc3bd6f986854625d8a475e9943b21
MahmoudAliKhider <mahmoudkhider37@gmail.com>
2023-03-14 18:35:10 +0200

    Add curd category and make a product & category model

--- Backend/routers/categories.js
+++ Backend/routers/categories.js
@@ -10,4 +10,37 @@ router.get("/", async (req, res) => {
   res.send(categoryList);
 });
 
+router.post("/", async (req, res) => {
+  let category = new categoryModel({
+    name: req.body.name,
+    icon: req.body.icon,
+    color: req.body.color,
+  });
+
+  category = await category.save();
+  if (!category) {
+    res.status(404).send("the category cant by created...");
+  }
+  res.json(category);
+});
+
+router.delete("/:id", (req, res) => {
+  categoryModel
+    .findByIdAndRemove(req.params.id)
+    .then((category) => {
+      if (category) {
+        return res
+          .status(200)
+          .json({ success: true, message: "the category will de deleted" });
+      } else {
+        return res
+          .status(404)
+          .json({ success: false, message: "category not found .!!" });
+      }
+    })
+    .catch((err) => {
+      return res.status(400).json({ success: false, error: err });
+    });
+});
+
 module.exports = router;
