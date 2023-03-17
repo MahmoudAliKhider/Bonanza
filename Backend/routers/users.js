749b34c6a2af82ddb5dac93d5b140c4af4e6d969
MahmoudAliKhider <mahmoudkhider37@gmail.com>
2023-03-17 02:20:50 +0200

    update user

--- Backend/routers/users.js
+++ Backend/routers/users.js
@@ -42,4 +42,33 @@ router.post("/", async (req, res) => {
   res.send(user);
 });
 
+router.put("/:id", async (req, res) => {
+  const exUser = await userModel.findById(req.params.id);
+  let newPassword;
+  if (req.body.password) {
+    newPassword = bcrypt.hashSync(req.body.password, 10);
+  } else {
+    newPassword = exUser.passwordHash;
+  }
+  const user = await userModel.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      email: req.body.email,
+      passwordHash: newPassword,
+      phone: req.body.phone,
+      isAdmin: req.body.isAdmin,
+      street: req.body.street,
+      apartment: req.body.apartment,
+      zip: req.body.zip,
+      city: req.body.city,
+      country: req.body.country,
+    },
+    { new: true }
+  );
+  if (!user) return res.status(400).send("the user cannot be created!");
+
+  res.send(user);
+});
+
 module.exports = router;
